9432164b3b7fa6cd3788cb7da1ae1b89eda11918
SanuSanal <=>
2024-11-25 17:48:57 +0000

    auto update changes

---
+++ lib/model/update_checker_model.dart
@@ -0,0 +1,13 @@
+class UpdateCheckerData {
+  final String version;
+  final String v8aDownloadUrl;
+  final String v7aDownloadUrl;
+  final String x86DownloadUrl;
+
+  UpdateCheckerData({
+    required this.version,
+    required this.v8aDownloadUrl,
+    required this.v7aDownloadUrl,
+    required this.x86DownloadUrl,
+  });
+}
--- lib/util/update_checker.dart
+++ lib/util/update_checker.dart
@@ -1,21 +1,46 @@
 import 'dart:convert';
+import 'dart:io';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
+import 'package:open_filex/open_filex.dart';
 import 'package:package_info_plus/package_info_plus.dart';
-import 'package:url_launcher/url_launcher.dart'; // For getting app version info
+import 'package:path_provider/path_provider.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:xstream/model/update_checker_model.dart'; // For getting app version info
 
 class UpdateChecker {
+  final Dio _dio = Dio();
+
   final String owner = 'SanuSanal';
   final String repo = 'xstream';
 
-  Future<String?> getLatestReleaseVersion() async {
+  Future<UpdateCheckerData?> getLatestReleaseVersion() async {
     final url = 'https://api.github.com/repos/$owner/$repo/releases/latest';
     try {
       final response = await http.get(Uri.parse(url));
       if (response.statusCode == 200) {
         final data = jsonDecode(response.body);
-        return data['tag_name'];
+        var latestVersion = data['tag_name'];
+        var v8aDownloadUrl = '';
+        var v7aDownloadUrl = '';
+        var x86DownloadUrl = '';
+        for (var asset in data['assets']) {
+          if ((asset['name'] as String).contains('v8a')) {
+            v8aDownloadUrl = asset['browser_download_url'];
+          } else if ((asset['name'] as String).contains('v7a')) {
+            v7aDownloadUrl = asset['browser_download_url'];
+          } else if ((asset['name'] as String).contains('x86_64')) {
+            x86DownloadUrl = asset['browser_download_url'];
+          }
+        }
+
+        return UpdateCheckerData(
+            version: latestVersion,
+            v8aDownloadUrl: v8aDownloadUrl,
+            v7aDownloadUrl: v7aDownloadUrl,
+            x86DownloadUrl: x86DownloadUrl);
       } else {
         return null;
       }
@@ -25,12 +50,15 @@ class UpdateChecker {
   }
 
   Future<void> checkForUpdate(BuildContext context) async {
-    final latestVersion = await getLatestReleaseVersion();
-    if (latestVersion == null) return;
+    final versionDetails = await getLatestReleaseVersion();
+    if (versionDetails == null) return;
+
+    var latestVersion = versionDetails.version;
 
     final packageInfo = await PackageInfo.fromPlatform();
     final currentVersion = packageInfo.version;
 
+    // if (_hasNewerVersion(currentVersion, latestVersion) && context.mounted) {
     if (_hasNewerVersion(currentVersion, latestVersion) && context.mounted) {
       showDialog(
         context: context,
@@ -51,8 +79,11 @@ class UpdateChecker {
                 onPressed: () {
                   Navigator.of(context).pop();
                   // Replace the URL with the download link of your app
-                  _launchURL('https://github.com/$owner/$repo/releases/latest',
-                      context);
+                  // _launchURL('https://github.com/$owner/$repo/releases/latest',
+                  //     context);
+
+                  _downloadAndInstallApk(
+                      context, versionDetails.v8aDownloadUrl);
                 },
               ),
             ],
@@ -73,6 +104,105 @@ class UpdateChecker {
         const SnackBar(
             content: Text('Url copied to clipboard. Open in a browser.')),
       );
+    }
+  }
+
+  Future<void> _downloadAndInstallApk(
+      BuildContext context, String apkUrl) async {
+    final progressNotifier = ValueNotifier<double>(0.0);
+    bool isDialogVisible = true;
+
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return Dialog(
+          backgroundColor: Colors.transparent,
+          child: Center(
+            child: ValueListenableBuilder<double>(
+              valueListenable: progressNotifier,
+              builder: (context, progress, child) {
+                return Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    CircularProgressIndicator(
+                      value: progress > 0 ? progress : null,
+                      strokeWidth: 6.0,
+                      color: Colors.white,
+                    ),
+                    const SizedBox(height: 20),
+                    Text(
+                      "${(progress * 100).toStringAsFixed(0)}%",
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                );
+              },
+            ),
+          ),
+        );
+      },
+    ).then((_) {
+      isDialogVisible = false;
+    });
+
+    try {
+      final directory = await getTemporaryDirectory();
+      final apkPath = '${directory.path}/update.apk';
+
+      if (context.mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text("Downloading update...")),
+        );
+      }
+
+      await _dio.download(
+        apkUrl,
+        apkPath,
+        onReceiveProgress: (received, total) {
+          if (total != -1) {
+            final progress = received / total;
+            progressNotifier.value = progress;
+
+            if (progress >= 1.0 && isDialogVisible) {
+              // Navigator.of(context).pop(); // Close the dialog
+              isDialogVisible = false;
+            }
+          }
+        },
+      );
+
+      File apkFile = File(apkPath);
+
+      if (apkFile.existsSync()) {
+        if (context.mounted) {
+          if (isDialogVisible) {
+            Navigator.of(context).pop(); // Close dialog on error
+          }
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+                content: Text("Download complete! Installing update...")),
+          );
+        }
+
+        await OpenFilex.open(apkPath);
+
+        File(apkPath).deleteSync();
+      } else {
+        if (context.mounted) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text("APK file not found!")),
+          );
+        }
+        throw Exception("APK file not found!");
+      }
+    } catch (e) {
+      if (context.mounted) {
+        _launchURL('https://github.com/$owner/$repo/releases/latest', context);
+      }
     }
   }
 
