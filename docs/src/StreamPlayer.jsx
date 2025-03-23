7c89ac5ae68d4fbfd5dc0c5332aef7ac9cd6bcab
SanuSanal <=>
2025-03-23 21:45:09 +0000

    adding an iframe to load the urls

---
+++ docs/src/StreamPlayer.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
+
+const StreamPlayer = () => {
+    const location = useLocation();
+    const params = new URLSearchParams(location.search);
+
+    const currentUrl = params.getAll('ref')[0] || "https://www.vipbox.lc";
+
+    return (
+        <div className="stream-container">
+            <iframe
+                src={currentUrl}
+                width="100%"
+                height="100%"
+                sandbox="allow-scripts allow-same-origin"
+                allow="fullscreen"
+            ></iframe>
+        </div>
+    );
+};
+
+export default StreamPlayer;
--- docs/src/main.jsx
+++ docs/src/main.jsx
@@ -5,12 +5,14 @@ import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import StreamPlayer from './StreamPlayer.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
      <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/load" element={<StreamPlayer />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
